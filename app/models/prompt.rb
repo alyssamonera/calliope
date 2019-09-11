11e2964053fd7a26ea5b61e44fb3820b80873212
alyssadm <ahlisa.dm@gmail.com>
2019-09-11 13:15:45 -0400

    encoded uri before parsing in production

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -1,6 +1,7 @@
 class Prompt
   if (ENV["DATABASE_URL"])
-      uri = URI.parse(ENV["DATABASE_URL"])
+      encoded_url = URI.encode(ENV["DATABASE_URL"])
+      uri = URI.parse(encoded_url)
       DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
     else
       DB = PG.connect(host: "localhost", port: 5432, dbname: "calliope_development")
